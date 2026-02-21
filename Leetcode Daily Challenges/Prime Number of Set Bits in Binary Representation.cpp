3e6749ba16bc673a4676e41d1a4ed1d8c1d356dd
Ahmad Raza <ahmadraza.og@gmail.com>
2026-02-21 22:11:30 +0500

    Add solution for counting prime set bits
    Implemented a function to count numbers with a prime number of set bits in a given range.

--- Leetcode Daily Challenges/Prime Number of Set Bits in Binary Representation.cpp
+++ Leetcode Daily Challenges/Prime Number of Set Bits in Binary Representation.cpp
@@ -1,5 +1,25 @@
-
-
+/******************************************************* Approach 1 *******************************************************/
+class Solution {
+bool isPrime(int n){
+        if(n < 2) return false;  
+        for(int i = 2; i < n; i++){
+            if(n % i == 0)
+                return false;
+        }
+        return true;
+    }
+public:
+    int countPrimeSetBits(int left, int right) {
+        int result = 0;
+        for(int i = left; i <= right;i++){
+            int count = __builtin_popcount(i);
+            if(isPrime(count)){
+                result++;
+            }
+        }
+        return result;
+    }
+};
 /******************************************************* Approach 2 *******************************************************/
 //Approach (Simple loop and check)
 //T.C : O((right - left) * log(num))
